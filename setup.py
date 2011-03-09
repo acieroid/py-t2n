ded80b63937ca66b70ed90c90ca4c41b111a5e3d
acieroid <none@none>
2011-03-09 17:00:16 +0100

    adding setup.py, license and renaming executable
    --HG--
    rename : t2n.py => py-t2n

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='py-t2n',
+      version='0.1',
+      description='Talk To NXT with Python',
+      author='Quentin Stievenart',
+      author_email='quentin.stievenart@gmail.com',
+      url='http://awesom.eu/~acieroid/py-t2n.html',
+      license='Gnu GPL v3',
+      scripts=['py-t2n'],
+      requires=['nxt']
+      )
